19a486ffea8f731234dedb28ec095d0414d0dc8f
Patrick <breen.patrick.1010@gmail.com>
2022-06-28 17:23:30 -0400

    full send

--- build_helm.sh
+++ build_helm.sh
@@ -1,3 +1,6 @@
 helm template wave2-infra-dev ./wave2-infra -f ./wave2-infra/dev-values.yaml > ./dev/wave2-infra/manifest.yaml
-git add .
+helm template wave3-app-tasks-dev ./wave3-app-tasks -f ./wave3-app-tasks/dev-values.yaml > ./dev/wave3-app-tasks/manifest.yaml
+git add dev/wave2-infra/manifest.yaml
+git add dev/wave3-app-tasks/manifest.yaml
+
 
