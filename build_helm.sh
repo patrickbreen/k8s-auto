af15c08f781bce826e00b1c5fe958e3a9e5d6e80
Patrick <breen.patrick.1010@gmail.com>
2022-06-30 17:23:44 -0400

    fix

--- build_helm.sh
+++ build_helm.sh
@@ -1,3 +1,4 @@
+#/bin/bash -e
 helm template wave2-infra-dev ./wave2-infra -f ./wave2-infra/dev-values.yaml > ./dev/wave2-infra/manifest.yaml
 helm template wave3-app-tasks-dev ./wave3-app-tasks -f ./wave3-app-tasks/dev-values.yaml > ./dev/wave3-app-tasks/manifest.yaml
 git add dev/wave2-infra/manifest.yaml
